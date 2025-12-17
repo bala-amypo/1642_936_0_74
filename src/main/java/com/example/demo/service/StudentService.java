f60ea4c038f3cb02feea4d3ddc5b29715f383b51
bala-amypo <bala@amypo.in>
2025-12-17 08:51:47 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,5 +1,9 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.StudentEntity;
 public interface StudentService{
     //We will give method definitions in other file. We're just declaring methods here. So interface
+
+    StudentEntity postData(StudentEntity stu);
+    //studentEntity is return type 
 }
