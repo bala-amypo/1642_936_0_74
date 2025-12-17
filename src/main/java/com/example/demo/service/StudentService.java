f2462524dea9a7967e7c978fc2002280c9852b8a
bala-amypo <bala@amypo.in>
2025-12-17 08:46:47 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,1 +1,5 @@
-package com.example.demo.service
+package com.example.demo.service;
+
+public interface StudentService{
+    //We will give method definitions in other file. We're just declaring methods here. So interface
+}
