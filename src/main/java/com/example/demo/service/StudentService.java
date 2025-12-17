1e1d3f9e7385eed039d794c2d46367905a1da8aa
bala-amypo <bala@amypo.in>
2025-12-17 09:41:48 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.StudentEntity;
+//We should not use @Service 
 public interface StudentService{
     //We will give method definitions in other file. We're just declaring methods here. So interface
 
