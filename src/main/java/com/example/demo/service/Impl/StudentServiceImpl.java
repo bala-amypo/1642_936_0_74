7d857271fa3189411307df297f04c9c33e1688ce
bala-amypo <bala@amypo.in>
2025-12-17 09:11:48 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
@@ -4,6 +4,8 @@ import com.example.demo.service.StudentService;
 
 @Service  //business logic layer
 public class StudentServiceImpl implements StudentService{
+  @Autwired StudentRepository student; //creating obj for StudentRepository to handle built in methods
+  
   public StudentEntity postData(StudentEntity stu){
     
   }
