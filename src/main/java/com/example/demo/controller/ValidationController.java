c790af5b5e7a3717d15cc0d607d44e19b86dbbaf
bala-amypo <bala@amypo.in>
2025-12-18 08:51:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/ValidationController.java
+++ src/main/java/com/example/demo/controller/ValidationController.java
@@ -8,24 +8,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 // import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.DeleteMapping;
 // import org.springframework.web.bind.annotation.PutMapping;
-import com.example.demo.entity.StudentEntity;
-import com.example.demo.service.StudentService;
+import com.example.demo.entity.ValidationEntity;
+import com.example.demo.service.ValidationService;
 import java.util.List;
 
 
 @RestController
-public class StudentController{
-   @Autowired StudentService ser;
+public class ValidationController{
+   @Autowired ValidationService serv;
 
    @PostMapping("/post")
-   public StudentEntity SendData(@RequestBody StudentEntity stu){
-       return ser.postData(stu);
+   public ValidationEntity SendData(@RequestBody ValidationEntity validate){
+       return serv.postData(stu);
    }
 
-   @GetMapping("/get")
-   public List<ValidationEntity> GetData(){
-        return ser.getAllData();
-   }
+//    @GetMapping("/get")
+//    public List<ValidationEntity> GetData(){
+//         return ser.getAllData();
+//    }
 
 //    @DeleteMapping("/delete/{id}")
 //    public String DeleteValue(@PathVariable int id){
