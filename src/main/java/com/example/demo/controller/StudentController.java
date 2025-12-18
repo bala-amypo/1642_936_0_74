50ae9403502d101fb2954ba979675b0dd65f096a
bala-amypo <bala@amypo.in>
2025-12-18 05:30:14 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -31,7 +31,7 @@ public class StudentController{
     return ser.deleteData(id);
    }
    
-   @GetMapping("/get1")
+   @GetMapping("/get1/{id}")
    public StudentEntity getData(@PathVariable int id){
     return ser.getDataById(id);
    }
