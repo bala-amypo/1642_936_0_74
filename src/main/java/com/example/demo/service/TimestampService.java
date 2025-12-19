01e50dda03aa4480844a1fdfcdfd2caccf72e01d
bala-amypo <bala@amypo.in>
2025-12-19 08:01:47 +0000

    User commit

--- src/main/java/com/example/demo/service/TimestampService.java
+++ src/main/java/com/example/demo/service/TimestampService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.entity.Timestamp;
 import java.util.List;
 
+@Service
 public interface Timestamp{
    
     Timestamp postStampData(Timestamp ts);   
