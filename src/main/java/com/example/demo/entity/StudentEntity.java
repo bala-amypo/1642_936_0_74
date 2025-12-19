fbde538a589f13e5a4a8b83a5080bce2ad086a27
bala-amypo <bala@amypo.in>
2025-12-19 06:28:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -16,47 +16,7 @@ public class StudentEntity{
           private String pwd;
           private Date created;
 
-          // public void setId(Integer id){
-          //   this.id=id;
-          // }
-          // public void setUsername(String username){
-          //   this.username=username;
-          // }
-          // public void setEmail(String email){
-          //   this.email=email;
-          // }
-          //  public void setPwd(String pwd){
-          //   this.pwd=pwd;
-          // }
-          // public void setCreated(Date created){
-          //   this.created=created;
-          // }
-          // public Integer getID(){
-          //   return id;
-          // }
-          // public String getUsername(){
-          //   return username;
-          // }
-          // public String getEmail(){
-          //   return email;
-          // }
-          // public String getPwd(){
-          //   return pwd;
-          // }
-          // public Date getCreated(){
-          //   return created;
-          // }
-          // public StudentEntity(Integer id,String username,String email,String pwd,Date created){
-          //   this.id=id;
-          //   this.username=username;
-          //   this.email=email;
-          //   this.pwd=pwd;
-          //   this.created=created;
-          // }
-          // public StudentEntity(){
-           
-          // }
 
-          
+
 
 }
--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -28,46 +28,5 @@ public class ValidationEntity{
           private int age;
       
 
-          public void setId(Long id){
-            this.id=id;
-          }
-          public void setUsername(String username){
-            this.username=username;
-          }
-          public void setEmail(String email){
-            this.email=email;
-          }
-           public void setPwd(String pwd){
-            this.pwd=pwd;
-          }
-          
-          public Long getID(){
-            return id;
-          }
-          public String getUsername(){
-            return username;
-          }
-          public String getEmail(){
-            return email;
-          }
-          public String getPwd(){
-            return pwd;
-          }
-          public int getAge(){
-            return age;
-          }
-
-          public ValidationEntity(Long id,
-          @NotNull @Size(min=2,max=10,message="Username must have atleast 2 to a maximum of 10 characters") String username,
-          @Email (message="Email is not valid") String email,
-          @Size(min=2,max=6,message="Must be 2 to 6 characters") @NotNull(message="Password is mandatory") String pwd,
-          @Max(30) @Positive(message="Age must be positive number") int age
-          ){
-            this.id=id;
-            this.username=username;
-            this.email=email;
-            this.pwd=pwd;
-            this.age=age;
-          }
-          public ValidationEntity(){}
+      
 }
