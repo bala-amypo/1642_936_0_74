bbcc1adbec1b710cf6d1904e127d41458b040ec0
bala-amypo <bala@amypo.in>
2025-12-19 12:36:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/Student1Controller.java
+++ src/main/java/com/example/demo/controller/Student1Controller.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 // import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.DeleteMapping;
 // import org.springframework.web.bind.annotation.PutMapping;
-import com.example.demo.entity.IdDetails;
-import com.example.demo.service.IdDetailsService;
+import com.example.demo.entity.StudentDetails;
+import com.example.demo.service.StudentDetailsService;
 // import java.util.List;
 
 
