2ebfe7e6a84a923989600cdf16feb66385a30aa4
bala-amypo <bala@amypo.in>
2025-12-19 05:31:34 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalException.java
+++ src/main/java/com/example/demo/exception/GlobalException.java
@@ -1,7 +1,12 @@
 package com.example.demo.exception;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 @RestControllerAdvice
 public class GlobalException{
+    @ExceptionHandler(ValidationException.class){
+        //This ValidationException has the custom error msg that has to be displayed in the UI
+        public String handleValidationException(){
 
+        }
+    }
 }
