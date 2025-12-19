bff3429d0147082427e5e2d7ddd966c49e5b1d4e
bala-amypo <bala@amypo.in>
2025-12-19 12:34:37 +0000

    User commit

--- src/main/java/com/example/demo/service/Student1Service.java
+++ src/main/java/com/example/demo/service/Student1Service.java
@@ -1,15 +1,10 @@
-package com.example.demo.service.Impl;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import com.example.demo.service.IdDetailsService;
+package com.example.demo.service;
 import com.example.demo.entity.StudentDetails;
-import com.example.demo.repository.IdDetailsRepository;
 // import java.util.List;
 
 
 public interface Student1Service{
  
-  public StudentDetails postIdDataa(StudentDetails ts);
+    StudentDetails postIdDataa(StudentDetails ts);
 
 }
