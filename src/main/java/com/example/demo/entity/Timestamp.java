93aab0189f4d5acbf09f8de3d00851ad77578286
bala-amypo <bala@amypo.in>
2025-12-19 07:43:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/Timestamp.java
+++ src/main/java/com/example/demo/entity/Timestamp.java
@@ -1,7 +1,24 @@
 package com.example.demo.entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Timestamp{
      @Id
    @GeneratedValue(strategy=GenerationType.IDENTITY)
    private Long id;
+
+   private String name;
+   private String email;
+   LocalDateTime createAt;
+   LocalDateTime updateAt;
+
+
 }
---
+++ src/main/java/com/example/demo/repository/TimestampRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.Timestamp;
+
+@Repository
+public interface StudentRepository extends JpaRepository<StudentEntity,Integer>{
+    //Here StudentEntity is the databse name //Integer is the datatype of primary key
+
+}
